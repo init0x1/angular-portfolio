47a9c24ca836f0cf7df20c14b2dbf127281bd21f
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-03-21 16:01:22 +0200

    adding the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from "./skills/skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
-
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

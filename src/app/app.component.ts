26f6967ed3390fb5db63ef135d16f9211585437c
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-03-21 14:09:39 +0200

    adding hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
